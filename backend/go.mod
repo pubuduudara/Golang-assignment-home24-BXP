9830c13fb96cd4eb24b7c3769f35e4e3ad7679a5
pubuduudara <pubuduudara7@gmail.com>
2025-04-17 00:48:02 +0530

    Add CORS middleware to the main router and update go.mod and go.sum for new dependency

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module github.com/pubuduudara/Golang-assignment-home24-BXP/backend
 go 1.24.2
 
 require (
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
 )
--- backend/internal/api/router.go
+++ backend/internal/api/router.go
@@ -4,11 +4,19 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/pubuduudara/Golang-assignment-home24-BXP/backend/internal/middleware"
 )
 
 func MainRouter() http.Handler {
 	router := chi.NewRouter()
+	// add CORS middleware first
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"}, // React dev server
+		AllowedMethods: []string{"GET"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-API-Key"},
+	}))
+	// add API key middleware
 	router.Use(middleware.APIKeyMiddleware)
 	router.Get("/analyze", analyzeHandler)
 	return router
