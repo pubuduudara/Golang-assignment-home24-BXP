7f912161c7d275d358118aceb3963f492dbc25a9
pubuduudara <pubuduudara7@gmail.com>
2025-04-17 09:16:56 +0530

    Refactor logger to use slog package for improved logging functionality

--- backend/internal/utils/logger/logger.go
+++ backend/internal/utils/logger/logger.go
@@ -1,27 +1,31 @@
 package logger
 
 import (
-	"log"
-	"time"
+	"log/slog"
+	"os"
 )
 
-// Info logs with timestamp
-func Info(message string) {
-	log.Printf("[INFO] %s | %s\n", time.Now().Format(time.RFC3339), message)
+var (
+	logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+)
+
+// Info logs an informational message
+func Info(msg string) {
+	logger.Info(msg)
 }
 
-// Warn logs with timestamp
-func Warn(message string) {
-	log.Printf("[WARN] %s | %s\n", time.Now().Format(time.RFC3339), message)
+// Warn logs a warning message
+func Warn(msg string) {
+	logger.Warn(msg)
 }
 
-// Error logs with timestamp
-func Error(err error, message ...string) {
+// Error logs an error message
+func Error(err error, msg ...string) {
 	if err != nil {
-		if len(message) > 0 {
-			log.Printf("[ERROR] %s | %s | %v\n", time.Now().Format(time.RFC3339), message[0], err)
+		if len(msg) > 0 {
+			logger.Error(msg[0], "error", err)
 		} else {
-			log.Printf("[ERROR] %s | %v\n", time.Now().Format(time.RFC3339), err)
+			logger.Error("Error occurred", "error", err)
 		}
 	}
 }
